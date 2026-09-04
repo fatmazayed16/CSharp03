7bd7630f7f7a14208df425856d300096b40f218d
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-04 16:34:05 +0300

    1st Answer

--- C#Basics03/Program.cs
+++ C#Basics03/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-         
+            #region 1st Answer
+            //Create a Book object and store it in a variable of type object. Print it.
+            Book book = new Book();
+            object obj = book;
+            Console.WriteLine(obj);
+            #endregion
 
         }
     }
