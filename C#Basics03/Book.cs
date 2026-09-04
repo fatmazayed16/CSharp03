c3f1d9eca8b92ad2755566eb8f09d180eea50c01
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-04 16:31:53 +0300

    Add project files.

---
+++ C#Basics03/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace C_Basics03;
+
+internal class Book
+{
+    public string Title { get; set; }
+    public int Pages { get; set; }
+}
---
+++ C#Basics03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C_Basics03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+        }
+    }
+}
